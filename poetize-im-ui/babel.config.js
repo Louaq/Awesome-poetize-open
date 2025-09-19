c900092733e1a6995843d8227bb4d1897d0a0be0
leapya <1795621606@qq.com>
2025-09-19 13:50:08 +0800

    fix: 聊天室移除控制台显示console.log注释问题

--- poetize-im-ui/babel.config.js
+++ poetize-im-ui/babel.config.js
@@ -9,5 +9,12 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator'
-  ]
+  ],
+  env: {
+    production: {
+      plugins: [
+        ['transform-remove-console', { exclude: ['error', 'warn'] }]
+      ]
+    }
+  }
 }
